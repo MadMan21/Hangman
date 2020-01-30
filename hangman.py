4386259e048ee3b5468a541340801110ca59d119
MadMan21 <manuelfresser21@gmail.com>
2020-01-30 09:45:58 +0100

    -day 2

--- hangman.py
+++ hangman.py
@@ -1,19 +1,43 @@
 import random
 
-Woerter = ['Krankenschwester', 'Finanzen', 'Handschelle', 'Essig', 'Rotwein']
+Woerter = ['Krankenschwester']
+'''Finanzen', 'Handschelle', 'Essig', 'Rotwein'''
 
 Wort = random.choice(Woerter)
 Wort = Wort.upper()
 # gets a random word out of our list
 
-newstring = list(Wort)
+newlist = list(Wort)
 # splits the random words into a list
-
-Eingabe = str(input ('Gib einen Buchstaben ein oder loese das Wort: '))
-Eingabe = Eingabe.upper()
-
+counter = 0
+guessed_right = False
 
 
-while Eingabe != Wort or :
-    for i in newstring:
-        print (i)
+
+
+while True :
+    counter = counter + 1
+    my_input = str(input ('Gib einen Buchstaben ein oder loese das Wort: '))
+    my_input = my_input.upper()
+
+    for item in newlist:
+        if my_input == item:
+            number_of_rights = newlist.count(my_input)
+            guessed_right = True
+        elif item != my_input:
+            guessed_right = False
+
+    if guessed_right == True:
+        print('yes')
+
+    elif guessed_right == False:
+        print('no')
+
+
+
+    '''if Eingabe in newstring:
+        print('yeah')
+
+
+    elif Eingabe not in newstring:
+        print('Auweh')'''
