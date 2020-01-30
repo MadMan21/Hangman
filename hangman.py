3e1c533257600c8ee307d834007f2ecd53f5e913
MadMan21 <manuelfresser21@gmail.com>
2020-01-30 10:31:00 +0100

    - day 2_ getting there, not so elegant, but works

--- hangman.py
+++ hangman.py
@@ -9,35 +9,43 @@ Wort = Wort.upper()
 
 newlist = list(Wort)
 # splits the random words into a list
-counter = 0
+trys_left = 10
 guessed_right = False
+number_of_rights = 0
 
 
 
 
 while True :
-    counter = counter + 1
+    trys_left = trys_left - 1
     my_input = str(input ('Gib einen Buchstaben ein oder loese das Wort: '))
     my_input = my_input.upper()
 
-    for item in newlist:
-        if my_input == item:
+    if len(my_input) == 1:
+
+        if my_input in newlist:
             number_of_rights = newlist.count(my_input)
             guessed_right = True
-        elif item != my_input:
+
+        else:
             guessed_right = False
 
-    if guessed_right == True:
-        print('yes')
+        if guessed_right == True:
+            trys_left = trys_left + 1
+            print('Yes you guessed', number_of_rights, 'letter(s) right, you got ', trys_left, 'trys left. ')
 
-    elif guessed_right == False:
-        print('no')
+        elif trys_left < 1:
+            print('You loose! GIT GUD!!!')
+            break
+
+        elif guessed_right == False:
+            print('no, thats wrong, guess again. You got ', trys_left, 'trys left.')
 
 
+    elif len(my_input) == len(Wort):
+        if Wort == my_input:
+            print ('Holy Moly, you did it!')
+            break
 
-    '''if Eingabe in newstring:
-        print('yeah')
-
-
-    elif Eingabe not in newstring:
-        print('Auweh')'''
+        else:
+            print('The length of the word is right, change the word!')
