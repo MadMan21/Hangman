0b1b949995268cfd2963d9cae7bf0e19b6356e56
MadMan21 <manuelfresser21@gmail.com>
2020-01-31 17:27:51 +0100

    -added a list for random words program now gets the words from "list.py".

--- hangman.py
+++ hangman.py
@@ -1,13 +1,13 @@
 import random
+from list import words
 
-Woerter = ['Krankenschwester', 'Finanzen', 'Handschelle', 'Essig', 'Rotwein']
-Wort = random.choice(Woerter)
+Wort = random.choice(words)
 Wort = Wort.upper()
 # gets a random word out of our list
 
 newlist = list(Wort)
 # splits the random words into a list
-trys_left = 100
+trys_left = 50
 guessed_right = False
 number_of_rights = 0
 guesslist = list('x'  * len(newlist))
@@ -16,14 +16,14 @@ guesslist = list('x'  * len(newlist))
 
 while True :
     if trys_left < 1:
-        print('You loose! GIT GUD!!!')
+        print('You loose! The word was',(Wort), '. GIT GUD!!!')
         break
 
     if guesslist == newlist:
         print('You got it by guessing. Nice!')
         break
 
-    trys_left = trys_left - 1
+
     my_input = str(input ('Enter a letter or solve the whole word. '))
     my_input = my_input.upper()
     print ('TRYS LEFT: ', (trys_left))
@@ -49,6 +49,7 @@ while True :
             guessstring = ''.join(guesslist)
             print (guessstring)
             print('No, thats wrong.')
+            trys_left = trys_left - 1
 
 
 
@@ -56,11 +57,9 @@ while True :
 
     elif len(my_input) == len(Wort):
         if Wort == my_input:
+            trys_left = trys_left + 1
             print ('Holy Moly, you did it!')
             break
 
         else:
             print('The length of the word is right, change the word!')
-
-def swap ():
-    guessstring[indizes] = my_input
---
+++ list.py
@@ -0,0 +1,4 @@
+
+
+with open('C:\\Users\manue\Desktop\Programmieren\Python\Hangman\woerter.txt') as file:
+    words = file.read().split()
